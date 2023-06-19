a8c26ba2f09194491b4b49ee3e0f2c0fdd530194
Mateusz <mateusz.grabarski@gmail.com>
2023-06-19 19:04:48 +0200

    Added lorem ipsum consts. Added about_image resource

--- site/src/jsMain/kotlin/org/example/empty/util/Constants.kt
+++ site/src/jsMain/kotlin/org/example/empty/util/Constants.kt
@@ -6,6 +6,9 @@ object Constants {
     const val FONT_FAMILY = "Roboto"
 }
 
+const val LOREM_IPSUM_SHORT = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+const val LOREM_IPSUM_LONG = LOREM_IPSUM_SHORT + LOREM_IPSUM_SHORT
+
 object Res {
 
     object Image {
@@ -13,5 +16,6 @@ object Res {
         const val background = "background.svg"
         const val logo = "logo.svg"
         const val main_image = "main_image.png"
+        const val about_image = "about_image.png"
     }
 }
