c7b8956aacdbdd1bc6cbf87d9071c504aa322502
Mateusz <mateusz.grabarski@gmail.com>
2023-06-19 19:05:25 +0200

    Added SkillBar

---
+++ site/src/jsMain/kotlin/org/example/empty/components/SkillBar.kt
@@ -0,0 +1,90 @@
+package org.example.empty.components
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.compose.css.FontWeight
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
+import com.varabyte.kobweb.compose.foundation.layout.Box
+import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.foundation.layout.Row
+import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
+import com.varabyte.kobweb.compose.ui.modifiers.color
+import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.fontFamily
+import com.varabyte.kobweb.compose.ui.modifiers.fontSize
+import com.varabyte.kobweb.compose.ui.modifiers.fontWeight
+import com.varabyte.kobweb.compose.ui.modifiers.height
+import com.varabyte.kobweb.compose.ui.modifiers.margin
+import com.varabyte.kobweb.compose.ui.modifiers.maxWidth
+import com.varabyte.kobweb.compose.ui.modifiers.padding
+import com.varabyte.kobweb.compose.ui.toAttrs
+import org.example.empty.models.Theme
+import org.example.empty.util.Constants.FONT_FAMILY
+import org.jetbrains.compose.web.css.CSSSizeValue
+import org.jetbrains.compose.web.css.CSSUnit
+import org.jetbrains.compose.web.css.percent
+import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.dom.P
+import org.jetbrains.compose.web.dom.Text
+
+@Composable
+fun SkillBar(
+    name: String,
+    progressBarHeight: CSSSizeValue<CSSUnit.px> = 5.px,
+    percentage: CSSSizeValue<CSSUnit.percent> = 50.percent
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .margin(bottom = 10.px)
+            .maxWidth(500.px)
+            .padding(topBottom = 5.px)
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .margin(bottom = 5.px),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            P(
+                attrs = Modifier
+                    .margin(topBottom = 0.px)
+                    .fontFamily(FONT_FAMILY)
+                    .fontSize(18.px)
+                    .fontWeight(FontWeight.Normal)
+                    .color(Theme.Secondary.rgb)
+                    .toAttrs()
+            ) {
+                Text(value = name)
+            }
+            P(
+                attrs = Modifier
+                    .margin(topBottom = 0.px)
+                    .fontFamily(FONT_FAMILY)
+                    .fontSize(18.px)
+                    .fontWeight(FontWeight.Normal)
+                    .color(Theme.Secondary.rgb)
+                    .toAttrs()
+            ) {
+                Text(value = "${percentage.value}${percentage.unit}")
+            }
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(size = progressBarHeight)
+                    .backgroundColor(Theme.LightGray.rgb)
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(percentage)
+                    .height(size = progressBarHeight)
+                    .backgroundColor(Theme.Primary.rgb)
+            )
+        }
+    }
+}
