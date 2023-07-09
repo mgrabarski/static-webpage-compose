93206a59c09999baeac0cf539841cc26e63aec6a
Mateusz <mateusz.grabarski@gmail.com>
2023-07-09 21:19:26 +0200

    Added BackToTopButton and OverflowMenu

--- site/src/jsMain/kotlin/org/example/empty/pages/Index.kt
+++ site/src/jsMain/kotlin/org/example/empty/pages/Index.kt
@@ -1,15 +1,24 @@
 package org.example.empty.pages
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
+import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.core.Page
+import org.example.empty.components.BackToTopButton
+import org.example.empty.components.OverflowMenu
 import org.example.empty.sections.AboutSection
 import org.example.empty.sections.AchievementsSection
+import org.example.empty.sections.ContactSection
 import org.example.empty.sections.ExperienceSection
+import org.example.empty.sections.FooterSection
 import org.example.empty.sections.MainSection
 import org.example.empty.sections.PortfolioSection
 import org.example.empty.sections.ServiceSection
@@ -18,17 +27,31 @@ import org.example.empty.sections.TestimonialSection
 @Page
 @Composable
 fun HomePage() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Top
+    var menuOpened by remember { mutableStateOf(false) }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
     ) {
-        MainSection()
-        AboutSection()
-        ServiceSection()
-        PortfolioSection()
-        AchievementsSection()
-        TestimonialSection()
-        ExperienceSection()
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            MainSection(onMenuClicked = { menuOpened = true })
+            AboutSection()
+            ServiceSection()
+            PortfolioSection()
+            AchievementsSection()
+            TestimonialSection()
+            ExperienceSection()
+            ContactSection()
+            FooterSection()
+        }
+        BackToTopButton()
+        if (menuOpened) {
+            OverflowMenu(onMenuClosed = { menuOpened = false })
+        }
     }
 }
