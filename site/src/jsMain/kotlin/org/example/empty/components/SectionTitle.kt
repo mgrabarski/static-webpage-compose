96bd7b32915debe2ed6bc94309e6ecb641ab63c8
Mateusz <mateusz.grabarski@gmail.com>
2023-07-09 21:19:42 +0200

    Added animations

--- site/src/jsMain/kotlin/org/example/empty/components/SectionTitle.kt
+++ site/src/jsMain/kotlin/org/example/empty/components/SectionTitle.kt
@@ -1,6 +1,12 @@
 package org.example.empty.components
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import com.varabyte.kobweb.compose.css.CSSTransition
 import com.varabyte.kobweb.compose.css.FontWeight
 import com.varabyte.kobweb.compose.css.TextAlign
 import com.varabyte.kobweb.compose.foundation.layout.Box
@@ -17,11 +23,16 @@ import com.varabyte.kobweb.compose.ui.modifiers.fontWeight
 import com.varabyte.kobweb.compose.ui.modifiers.height
 import com.varabyte.kobweb.compose.ui.modifiers.margin
 import com.varabyte.kobweb.compose.ui.modifiers.textAlign
+import com.varabyte.kobweb.compose.ui.modifiers.transition
 import com.varabyte.kobweb.compose.ui.modifiers.width
 import com.varabyte.kobweb.compose.ui.toAttrs
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.example.empty.models.Section
 import org.example.empty.models.Theme
 import org.example.empty.util.Constants.FONT_FAMILY
+import org.example.empty.util.ObserveViewportEntered
+import org.jetbrains.compose.web.css.ms
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.P
 import org.jetbrains.compose.web.dom.Text
@@ -32,6 +43,24 @@ fun SectionTitle(
     section: Section,
     alignment: Alignment.Horizontal = Alignment.Start
 ) {
+    val scope = rememberCoroutineScope()
+    var titleMargin by remember { mutableStateOf(50.px) }
+    var subtitleMargin by remember { mutableStateOf(50.px) }
+
+    ObserveViewportEntered(
+        sectionId = section.id,
+        distanceFromTop = 700.0,
+        onViewPortEntered = {
+            scope.launch {
+                subtitleMargin = 0.px
+                if (alignment == Alignment.Start) {
+                    delay(25)
+                }
+                titleMargin = 0.px
+            }
+        }
+    )
+
     Column(
         modifier = modifier,
         horizontalAlignment = alignment
@@ -46,11 +75,18 @@ fun SectionTitle(
                         else -> TextAlign.Start
                     }
                 )
-                .margin(topBottom = 0.px)
+                .margin(
+                    top = 0.px,
+                    bottom = 0.px,
+                    leftRight = titleMargin
+                )
                 .fontFamily(FONT_FAMILY)
                 .fontSize(25.px)
                 .fontWeight(FontWeight.Normal)
                 .color(Theme.Primary.rgb)
+                .transition(
+                    CSSTransition(property = "margin", duration = 300.ms)
+                )
                 .toAttrs(),
         ) {
             Text(value = section.title)
@@ -66,11 +102,19 @@ fun SectionTitle(
                         else -> TextAlign.Start
                     }
                 )
-                .margin(bottom = 10.px, top = 0.px)
+                .margin(
+                    bottom = 10.px,
+                    top = 0.px,
+                    left = if (alignment == Alignment.Start) subtitleMargin else 0.px,
+                    right = if (alignment == Alignment.CenterHorizontally) subtitleMargin else 0.px,
+                )
                 .fontFamily(FONT_FAMILY)
                 .fontSize(40.px)
                 .fontWeight(FontWeight.Bold)
                 .color(Theme.Secondary.rgb)
+                .transition(
+                    CSSTransition(property = "margin", duration = 300.ms)
+                )
                 .toAttrs(),
         ) {
             Text(value = section.subtitle)
